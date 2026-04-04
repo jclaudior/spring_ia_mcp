8646048630a090cd225b02394a276c026a4b1df8
jclaudior <joaoclaudiodiva@gmail.com>
2026-04-03 21:43:00 -0300

    add company tools

--- src/main/java/com/jcr/mcp/application/adapter/out/client/CompanyClientPort.java
+++ src/main/java/com/jcr/mcp/application/adapter/out/client/CompanyClientPort.java
@@ -2,7 +2,6 @@ package com.jcr.mcp.application.adapter.out.client;
 
 import com.jcr.mcp.domain.model.PaginationCompanyDTO;
 
-import java.util.List;
 
 public interface CompanyClientPort {
     PaginationCompanyDTO getByUser(String userId);
